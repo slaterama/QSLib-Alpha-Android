8e554a9ee1e1d6e76b7f4e2a942c3bd71eed0eab
Scott Slater <slaterama@gmail.com>
2014-08-19 09:35:20 -0400

    Created a UserVO & view

--- src/main/java/com/slaterama/qslib/alpha/app/pattern/Model.java
+++ src/main/java/com/slaterama/qslib/alpha/app/pattern/Model.java
@@ -1,4 +1,56 @@
 package com.slaterama.qslib.alpha.app.pattern;
 
-public abstract class Model {
+import java.util.Observable;
+
+public abstract class Model extends Observable {
+
+	public static class ModelEvent {
+		private String mAction;
+		private Object mWhat;
+		private String mProperty;
+		private Object mValue;
+
+		public ModelEvent(String action, Object what, String property, Object value) {
+			mAction = action;
+			mWhat = what;
+			mProperty = property;
+			mValue = value;
+		}
+
+		public ModelEvent(String action, Object value) {
+			this(action, null, null, value);
+		}
+
+		public String getAction() {
+			return mAction;
+		}
+
+		public void setAction(String action) {
+			mAction = action;
+		}
+
+		public Object getWhat() {
+			return mWhat;
+		}
+
+		public void setWhat(Object what) {
+			mWhat = what;
+		}
+
+		public String getProperty() {
+			return mProperty;
+		}
+
+		public void setProperty(String property) {
+			mProperty = property;
+		}
+
+		public Object getValue() {
+			return mValue;
+		}
+
+		public void setValue(Object value) {
+			mValue = value;
+		}
+	}
 }
--- src/main/java/com/slaterama/qslib/alpha/app/pattern/mvp/Presenter.java
+++ src/main/java/com/slaterama/qslib/alpha/app/pattern/mvp/Presenter.java
@@ -1,8 +1,12 @@
 package com.slaterama.qslib.alpha.app.pattern.mvp;
 
 import com.slaterama.qslib.alpha.app.pattern.Model;
+import com.slaterama.qslib.alpha.app.pattern.Model.ModelEvent;
 
-public abstract class Presenter {
+import java.util.Observable;
+import java.util.Observer;
+
+public abstract class Presenter implements Observer {
 
 	protected IView mView;
 	protected Model mModel;
@@ -14,6 +18,16 @@ public abstract class Presenter {
 
 	protected void setModel(Model model) {
 		mModel = model;
+	}
+
+	@Override
+	public void update(Observable observable, Object data) {
+		if (observable instanceof Model && data instanceof ModelEvent)
+			update((Model) observable, (ModelEvent) data);
+	}
+
+	public void update(Model model, ModelEvent event) {
+
 	}
 
 	public static abstract interface IView {
