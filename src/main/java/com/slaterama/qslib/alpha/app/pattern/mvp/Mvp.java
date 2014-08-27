9dffaef62a870537c68ea0f77be999185a2a5d0f
Scott Slater <slaterama@gmail.com>
2014-08-26 22:20:42 -0400

    Added isPresenterRegistered method to Mvp

--- src/main/java/com/slaterama/qslib/alpha/app/pattern/mvp/Mvp.java
+++ src/main/java/com/slaterama/qslib/alpha/app/pattern/mvp/Mvp.java
@@ -3,15 +3,29 @@ package com.slaterama.qslib.alpha.app.pattern.mvp;
 import com.slaterama.qslib.alpha.app.pattern.Model;
 import com.slaterama.qslib.alpha.app.pattern.Pattern;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Mvp<M extends Model> extends Pattern<M> {
 
+	public Set<Presenter<M, ?>> mRegisteredPresenters;
+
 	public void registerPresenter(Presenter<M, ?> presenter) {
+		if (mRegisteredPresenters == null)
+			mRegisteredPresenters = new HashSet<>();
+		mRegisteredPresenters.add(presenter);
 		presenter.setModel(mModel);
 		mModel.addObserver(presenter);
 	}
 
 	public void unregisterPresenter(Presenter<M, ?> presenter) {
+		if (mRegisteredPresenters != null)
+			mRegisteredPresenters.remove(presenter);
 		presenter.setModel(null);
 		mModel.deleteObserver(presenter);
+	}
+
+	public boolean isPresenterRegistered(Presenter<M, ?> presenter) {
+		return (mRegisteredPresenters != null && mRegisteredPresenters.contains(presenter));
 	}
 }
